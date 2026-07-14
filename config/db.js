f19d8e16c8ed41fac0a8d4b67d48c2ec79c76a15
Snehadhir <Snehadhir7998@gmail.com>
2026-07-14 12:01:55 +0530

    Add .gitignore and remove node_modules

--- config/db.js
+++ config/db.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 10000
-});
+        console.log("URI:", process.env.MONGO_URI);
+
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error("❌ MongoDB Connection Failed");
         console.error(error);
         process.exit(1);
     }
